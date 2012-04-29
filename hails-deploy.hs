09ce04624a2a8f7a18195be28b9dc1c1594a246b
Deian Stefan <deian@cs.stanford.edu>
2012-04-28 19:57:20 -0700

    building app and launching app works

--- hails-deploy.hs
+++ hails-deploy.hs
@@ -19,6 +19,7 @@ import System.FilePath
 import System.Posix.Temp
 import System.IO
 import System.Process
+import System.Environment
 import System.Exit
 
 import Data.Maybe
@@ -26,6 +27,28 @@ import Data.List ((\\))
 
 import Control.Exception
 import Control.Monad
+
+--
+-- Trusted packages
+--
+
+-- | Core hails packages
+coreHailsPackages :: [String]
+coreHailsPackages = [ "dclabel", "lio"
+                    , "iterIO" , "iterio-server"
+                    , "hails"  , "hails-cjail"]
+
+-- | Packages trusted by hails
+otherTrustedPackages :: [String]
+otherTrustedPackages = [ "aeson"
+                       , "base64-bytestring"
+                       , "blaze-html"
+                       , "pandoc" ]
+
+
+--
+-- Cabal file related
+--
 
 -- | Find .cabal file in given directory
 findCabalFile :: FilePath -> IO FilePath
@@ -37,7 +60,7 @@ findCabalFile dir = do
         _ -> throwIO . userError $ "Expecting 1 cabal file"
 
 -- | Rewrite the given cabal file, creating a .bak backup
-rewriteCabalFile :: FilePath -> IO ()
+rewriteCabalFile :: FilePath -> IO ModuleName
 rewriteCabalFile file = do
   gpkg0 <- readPackageDescription normal file
   let pkg0 = packageDescription gpkg0
@@ -78,6 +101,7 @@ rewriteCabalFile file = do
                                 [(exeName exe, CondNode exe depsWCore [])] }
           copyFile file (file ++ ".bak")
           writeGenericPackageDescription file gpkg2
+          return moduleName
         _ -> throwIO . userError $ "Expecting top-level Library"
 
 -- | Write a dummy make file. The goal behind the file is to get the
@@ -104,22 +128,8 @@ addCoreHailsPackages ds =
                 then Dependency (PackageName n) anyVersion else d
   in map xfm ds
 
-
--- | Core hails packages
-coreHailsPackages :: [String]
-coreHailsPackages = [ "dclabel", "lio"
-                    , "iterIO" , "iterio-server"
-                    , "hails"  , "hails-cjail"]
-
--- | Packages trusted by hails
-otherTrustedPackages :: [String]
-otherTrustedPackages = [ "aeson"
-                       , "base64-bytestring"
-                       , "blaze-html"
-                       , "pandoc" ]
-
 --
---
+-- Cabal dev related
 --
 
 -- | Execute cabal-dev. If the process terminates abnormally, throw
@@ -138,12 +148,13 @@ copyBaseCabalDevDir base new = do
   (ex, _, _) <- readProcessWithExitCode "cp" ["-r", base, new] ""
   when (ex /= ExitSuccess) $ throwIO . userError $ "Copy base cabal-dev failed"
 
-main = do
-  let appDir          = "/tmp/app"
-      base_cabal_dir  = "/tmp/base_cabal_dev"
+-- | Copy the base cabal-dev environment and build the app
+buildApp :: FilePath -> IO ModuleName
+buildApp appDir = do
+  base_cabal_dir <- getEnv "HAILS_BASE_CABAL_DEV"
   inDir appDir $ do
     -- Rewrite cabal file
-    findCabalFile "." >>= rewriteCabalFile 
+    moduleName <- findCabalFile "." >>= rewriteCabalFile 
     -- Make sure there is no cabal-dev set up:
     exists <- doesDirectoryExist "cabal-dev"
     when exists $ throwIO . userError $ "cabal-dev sandbox must not exist"
@@ -155,6 +166,45 @@ main = do
     cabal_dev Nothing "configure" []
     -- Build package:
     cabal_dev Nothing "build" []
+    return moduleName
+
+--
+--
+--
+
+-- | Use git to clone the app contents into the /user/appName
+initApp :: String -> String -> String -> IO ()
+initApp gitRepo user appName = do
+  base_app_dir <- getEnv "HAILS_BASE_APP_DIR"
+  let dir = base_app_dir </> user </> appName
+  ignoreErr $ createDirectory $ base_app_dir </> user 
+  createDirectory dir
+  (ex, _, _) <- readProcessWithExitCode "git" ["clone", gitRepo, dir] ""
+  when (ex /= ExitSuccess) $ throwIO . userError $ "Cloning app failed"
+
+-- | Wrapper for calling 'initApp' and 'buildApp'. It clones
+-- the app and builds it.
+initAndBuildApp :: String -> String -> String -> IO ModuleName
+initAndBuildApp gitRepo user appName = do
+  base_app_dir <- getEnv "HAILS_BASE_APP_DIR"
+  initApp gitRepo user appName
+  buildApp $ base_app_dir </> user </> appName
+ 
+-- | Run the app within hails, the environment variables must be set
+-- accordingly, especially \"PACKAGE_CONF\", which should be set to
+-- \"cabal-dev/packages-7.4.1.conf\"
+runApp :: [(String, String)] -> FilePath -> IO ()
+runApp appEnv appDir = do
+  inDir appDir $ do
+    let hails = (proc "hails" []) { env = Just appEnv, cwd = Just appDir }
+    (_,_,_, ph) <- createProcess hails
+    ex <- waitForProcess ph
+    when (ex /= ExitSuccess) $ throwIO . userError $ "Deploying app falied"
+    
+
+--
+-- Misc
+--
 
 
 -- | Execute an action in a directory
